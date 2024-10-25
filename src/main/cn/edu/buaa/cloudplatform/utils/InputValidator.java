309d7a93d9f8ccc7330059790a89b59df19d1a15
YueChum <r1824367738@outlook.com>
2024-10-25 20:31:01 +0800

    修复了姓名、密码合法性的判断

--- src/main/cn/edu/buaa/cloudplatform/utils/InputValidator.java
+++ src/main/cn/edu/buaa/cloudplatform/utils/InputValidator.java
@@ -2,10 +2,10 @@ package main.cn.edu.buaa.cloudplatform.utils;
 
 public class InputValidator {
     public static boolean isValidName(String name) {
-        return name != null && name.matches("^[a-zA-Z_]{4,16}$");
+        return name != null && name.matches("^[a-zA-Z][a-zA-Z_]{3,15}$");
     }
 
     public static boolean isValidPassword(String password) {
-        return password != null && password.matches("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@_%$]).{6,16}$");
+        return password != null && password.matches("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@_%$])[a-zA-Z\\d@_%$]{6,16}$");
     }
 }
