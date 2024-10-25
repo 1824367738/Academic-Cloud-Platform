f211a298325a5c44fe1c30a4e3b2a698653da85b
YueChum <r1824367738@outlook.com>
2024-10-25 20:18:13 +0800

    修复了学生id合法性的判断

--- src/main/cn/edu/buaa/cloudplatform/utils/IdValidator.java
+++ src/main/cn/edu/buaa/cloudplatform/utils/IdValidator.java
@@ -2,7 +2,19 @@ package main.cn.edu.buaa.cloudplatform.utils;
 
 public class IdValidator {
     public static boolean isValidStudentId(String id) {
-        return id != null && id.matches("^\\d{8}$");
+        if(id != null && id.matches("^(19|20|21|22|23|24)(0[1-9]|[1-3][0-9]|4[0-3])[1-6](00[1-9]|0[1-9][0-9]|[1-9][0-9]{2})}$")){
+            //本科生
+            return true;
+        }
+        else if(id != null && id.matches("^(SY|ZY)(21|22|23|24)(0[1-9]|[1-3][0-9]|4[0-3])[1-6](0[1-9]|[1-9][0-9])$")){
+            return true;
+        }
+        else if(id != null && id.matches("^BY(19|20|21|22|23|24)(0[1-9]|[1-3][0-9]|4[0-3])[1-6](0[1-9]|[1-9][0-9])$")){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     public static boolean isValidTeacherId(String id) {
